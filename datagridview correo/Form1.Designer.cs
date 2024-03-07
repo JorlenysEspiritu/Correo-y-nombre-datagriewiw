10414795b34a8d4e55eb608b8d0793249127032b
JorlenysEspiritu <158392518+JorlenysEspiritu@users.noreply.github.com>
2024-03-06 22:30:58 -0400

    Add files via upload

---
+++ datagridview correo/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace datagridview_correo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_agregar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_cerrar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgv_nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgv_correo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_correo = new System.Windows.Forms.TextBox();
+            this.btn_Limpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 51);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(308, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(189, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Correo Electronico";
+            // 
+            // btn_agregar
+            // 
+            this.btn_agregar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_agregar.Location = new System.Drawing.Point(782, 114);
+            this.btn_agregar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_agregar.Name = "btn_agregar";
+            this.btn_agregar.Size = new System.Drawing.Size(177, 45);
+            this.btn_agregar.TabIndex = 3;
+            this.btn_agregar.Text = "Agregar";
+            this.btn_agregar.UseVisualStyleBackColor = false;
+            this.btn_agregar.Click += new System.EventHandler(this.btn_agregar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_eliminar.Location = new System.Drawing.Point(782, 168);
+            this.btn_eliminar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(177, 45);
+            this.btn_eliminar.TabIndex = 4;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = false;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_cerrar
+            // 
+            this.btn_cerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_cerrar.Location = new System.Drawing.Point(782, 275);
+            this.btn_cerrar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_cerrar.Name = "btn_cerrar";
+            this.btn_cerrar.Size = new System.Drawing.Size(177, 45);
+            this.btn_cerrar.TabIndex = 5;
+            this.btn_cerrar.Text = "Cerrar";
+            this.btn_cerrar.UseVisualStyleBackColor = false;
+            this.btn_cerrar.Click += new System.EventHandler(this.btn_cerrar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgv_nombre,
+            this.dgv_correo});
+            this.dataGridView1.Location = new System.Drawing.Point(0, 222);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.Size = new System.Drawing.Size(774, 226);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // dgv_nombre
+            // 
+            this.dgv_nombre.HeaderText = "Nombre";
+            this.dgv_nombre.MinimumWidth = 8;
+            this.dgv_nombre.Name = "dgv_nombre";
+            this.dgv_nombre.ReadOnly = true;
+            this.dgv_nombre.Width = 200;
+            // 
+            // dgv_correo
+            // 
+            this.dgv_correo.HeaderText = "Correo";
+            this.dgv_correo.MinimumWidth = 8;
+            this.dgv_correo.Name = "dgv_correo";
+            this.dgv_correo.ReadOnly = true;
+            this.dgv_correo.Width = 300;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombre.Location = new System.Drawing.Point(27, 120);
+            this.txt_nombre.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(148, 39);
+            this.txt_nombre.TabIndex = 7;
+            this.txt_nombre.TextChanged += new System.EventHandler(this.txt_nombre_TextChanged);
+            // 
+            // txt_correo
+            // 
+            this.txt_correo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_correo.Location = new System.Drawing.Point(313, 120);
+            this.txt_correo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt_correo.Name = "txt_correo";
+            this.txt_correo.Size = new System.Drawing.Size(256, 39);
+            this.txt_correo.TabIndex = 8;
+            // 
+            // btn_Limpiar
+            // 
+            this.btn_Limpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btn_Limpiar.Location = new System.Drawing.Point(782, 222);
+            this.btn_Limpiar.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btn_Limpiar.Name = "btn_Limpiar";
+            this.btn_Limpiar.Size = new System.Drawing.Size(177, 45);
+            this.btn_Limpiar.TabIndex = 9;
+            this.btn_Limpiar.Text = "Limpiar";
+            this.btn_Limpiar.UseVisualStyleBackColor = false;
+            this.btn_Limpiar.Click += new System.EventHandler(this.btn_Limpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(982, 480);
+            this.Controls.Add(this.btn_Limpiar);
+            this.Controls.Add(this.txt_correo);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_cerrar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_agregar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_agregar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_cerrar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgv_correo;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_correo;
+        private System.Windows.Forms.Button btn_Limpiar;
+    }
+}
+
---
+++ datagridview correo/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datagridview_correo
+{
+    public partial class Form1 : Form
+    {
+         DialogResult respuesta;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_agregar_Click(object sender, EventArgs e)
+        {
+
+           
+
+            if (txt_nombre.Text == ""|| txt_correo.Text == "")
+            {
+                respuesta = MessageBox.Show("El campo Nombre esta vacio, Debes de llenarlo", "Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                
+            }
+            else
+            {
+                dataGridView1.Rows.Add(txt_nombre.Text, txt_correo.Text);
+                txt_correo.Clear();
+                txt_nombre.Clear();
+                txt_nombre.Focus(); 
+            }
+            
+            
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            int fila;
+            if (dataGridView1.Rows.Count == 0)
+            {
+                 MessageBox.Show("No hay datos para eliminar", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+
+            else 
+            {
+               DialogResult respuesta = MessageBox.Show("Desea eliminar este registro?", "Registro", MessageBoxButtons.YesNo);
+
+                if (respuesta == DialogResult.Yes)
+                {
+                   
+                    fila = dataGridView1.CurrentRow.Index;
+                    dataGridView1.Rows.RemoveAt(fila);
+                    MessageBox.Show("Registro eliminado", "Registro");
+                }
+            }
+           
+            
+        }
+
+        private void btn_cerrar_Click(object sender, EventArgs e)
+        {
+            respuesta = MessageBox.Show("¿Quieres salir del programa?", "Programa", MessageBoxButtons.YesNo);
+            Close();
+        }
+
+        private void btn_Limpiar_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            txt_correo.Clear();
+            txt_nombre.Clear();
+        }
+
+        private void txt_nombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
